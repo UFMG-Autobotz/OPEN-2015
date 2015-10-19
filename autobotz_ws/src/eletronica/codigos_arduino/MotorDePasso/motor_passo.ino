6db2d36a206541c024f870a4df636381703898d1
pedro <pedro6893@gmail.com>
2015-10-19 13:42:30 -0200

    Motor de passo funcionando nos dois sentidos!

--- autobotz_ws/src/eletronica/codigos_arduino/MotorDePasso/motor_passo.ino
+++ autobotz_ws/src/eletronica/codigos_arduino/MotorDePasso/motor_passo.ino
@@ -5,6 +5,8 @@
 // e que a informacao de rotacao esta sendo publicada no topico
 // "/eletronica/base/stepper" como um inteiro cujo valor vai
 // de -255 a 255, onde 0 é parado
+//
+// Para rodar: rosrun rosserial_python serial_node.py /dev/ttyACM2
 
 #include <ros.h>
 #include <std_msgs/Int32.h>
@@ -17,18 +19,21 @@ const int stepper_pins[] = {8, 9, 10, 11}; //pinos nos quais esta o stepper
                                           //Os pinos sao ativados na ordem
                                           //desse vetor
 
-const int T_min = 100;  //menor periodo (|vel| = 255) em ms
+const int T_stepper_min = 100;  //menor periodo (|vel| = 255) em ms
                        //1 periodo = ciclo de ativacao dos 4 pinos
-const int T_max = 1000;
+const int T_stepper_max = 1000;
 
-/////// vairaveis para o ROS
+int T_stepper_atual = 0; //periodo a ser setado a partir do topico do ROS
+int dir;      //direcao do stepper 1 = frente, -1 = tras
+
+int T_ROS_spin = 30;  //periodo em ms entre uma chamada do ROS e outra
+
+////////////////      parte do ROS    ///////////////////
 ros::NodeHandle nh;
 void messageCb(const std_msgs::Int32& msg);
 const char* ROS_stepper_topic = "/eletronica/base/stepper";
 ros::Subscriber<std_msgs::Int32> sub(ROS_stepper_topic, &messageCb);
 
-int T_atual = 0; //periodo a ser setado a partir do topico do ROS
-int dir;      //direcao do stepper
 
 //callback para pegar o valor de velocidade no topico
 void messageCb(const std_msgs::Int32& msg)
@@ -49,16 +54,15 @@ void messageCb(const std_msgs::Int32& msg)
 	}
 	
 	if(vel == 0)
-		T_atual = INF;
+		T_stepper_atual = INF;
 	else
-		T_atual = ( 1 - float(abs(vel))/255 )*(T_max - T_min) + T_min;  //faz com que o periodo seja uma rampa linear
+		T_stepper_atual = ( 1 - float(abs(vel))/255 )*(T_stepper_max - T_stepper_min) + T_stepper_min;  //faz com que o periodo seja uma rampa linear
 }
 
-void step()   //faz um passo do stepper
-{	
-        static int indice = 0;    
-  
-	int atual = stepper_pins[indice];
+////////////////////    parte do stepper    ////////////////////
+
+void step_forward(int& indice)
+{
 	int proximo;
 	if(indice >= 3)
         {
@@ -66,43 +70,86 @@ void step()   //faz um passo do stepper
                 indice = 0;
         }
 	else
-        {
+	{
 		proximo = stepper_pins[indice + 1];
                 indice++;
         }
         
-	digitalWrite(atual, LOW);
+	for(int i = 0; i < 4; i++)
+	  digitalWrite(stepper_pins[i], LOW);
+	
+        digitalWrite(proximo, HIGH);
+ 
+        digitalWrite(13, HIGH - digitalRead(13));
+}
+
+void step_back(int & indice)
+{
+	int proximo;
+	if(indice <= 0)
+    {
+		proximo = stepper_pins[3];
+        indice = 3;
+    }
+	else
+	{
+		proximo = stepper_pins[indice - 1];
+        indice--;
+    }
+
+	for(int i = 0; i < 4; i++)
+		digitalWrite(stepper_pins[i], LOW);
 	digitalWrite(proximo, HIGH);
 }
 
-unsigned long time_start;
+void step()   //faz um passo do stepper
+{	
+    static int indice = 0;   
+  
+    if(dir > 0)
+      step_forward(indice);
+    else if(dir < 0)
+      step_back(indice);
+}
+
+//variaveis para temporizacao
+unsigned long time_start_stepper;
+unsigned long time_start_ROS;
 
 void setup()
-{
-	for(int i = 0; i < 4; i++)
-		pinMode(stepper_pins[i], OUTPUT);
-	
+{	
+    for(int i = 0; i < 4; i++)
+    {
+        pinMode(stepper_pins[i], OUTPUT);
+        digitalWrite(stepper_pins[i], LOW);
+    }
+
 	nh.initNode();
 	nh.subscribe(sub);
 
-        time_start = micros();
+    time_start_stepper = micros();
+	time_start_ROS = micros();
 
     pinMode(13, OUTPUT);     //for debug only
     digitalWrite(13, HIGH);  // 
 }
 
- 
+
 void loop()
 {
- 	nh.spinOnce();
+	if((micros() - time_start_ROS)/1000 >= T_ROS_spin)
+	{
+ 		nh.spinOnce();
+ 		time_start_ROS = micros();
+	}
  	
 	//se já passou um quarto de periodo
-	if((micros() - time_start)/1000 >= T_atual/4)
+	if((micros() - time_start_stepper)/1000 >= T_stepper_atual/4)
  	{
  		step();
- 		time_start = micros();
+ 		time_start_stepper = micros();
  	}
- 
-       delay(50);
+ 	
+ 	delay(1);
 }
 
